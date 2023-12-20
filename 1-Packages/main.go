9f59872c49db2e131116eca59f5841b0cfe56144
Bea-Trix1 <beatriz.ferrante37@gmail.com>
2023-12-20 12:23:34 -0300

    External packages

--- 1-Packages/main.go
+++ 1-Packages/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 	"module/auxiliary"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Writing main file")
 	auxiliary.Write()
 	auxiliary.Write2()
+	erro := checkmail.ValidateFormat("beatriz.ferrante@gmail.com")
+	fmt.Print(erro)
 }
