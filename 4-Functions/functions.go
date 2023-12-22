d8277f8bb98bba28c8cebd5dea8d50daf713a322
Bea-Trix1 <beatriz.ferrante37@gmail.com>
2023-12-22 13:20:52 -0300

    Functions

---
+++ 4-Functions/functions.go
@@ -0,0 +1,15 @@
+package main
+
+func add(n1 int8, n2 int8) int8 {
+	return n1 + n2
+}
+
+func calc(n1, n2 int8) (int8, int8) {
+	add := n1 + n2
+	subtr := n1 - n2
+	return add, subtr
+}
+
+func main() {
+
+}
